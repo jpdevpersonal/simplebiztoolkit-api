0443280c17a153793cff15e8fe5386b6c372525f
Julian <176511067+jpdevpersonal@users.noreply.github.com>
2026-09-22 22:59:27 +0100

    Revert "Added menu controllers and model"
    This reverts commit 9b33a182e3706c295870ed05b5e4a8732f9b196f.

--- Dtos/CreateMenuCategoryDto.cs
+++
@@ -1,9 +0,0 @@
-namespace simplebiztoolkit_api.Dtos;
-
-public class CreateMenuCategoryDto
-{
-    public Guid MenuItemId { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string? Description { get; set; }
-    public string Status { get; set; } = "draft";
-}
